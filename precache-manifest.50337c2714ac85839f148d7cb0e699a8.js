11b2858081d323c0d3c0c8c4b28cb006f46d5eb2
Ihor Stavratii <igor.stavratiy@gmail.com>
2020-01-13 23:34:21 +0200

    Updates

---
+++ precache-manifest.50337c2714ac85839f148d7cb0e699a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659b3940be6d0443e1dca8461903a15c",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "eb242db2f2303e0bb06b",
+    "url": "/react_uber-eats/static/css/main.44d56b38.chunk.css"
+  },
+  {
+    "revision": "02ed99eaf2f0bf4c4e61",
+    "url": "/react_uber-eats/static/js/2.9527cd1f.chunk.js"
+  },
+  {
+    "revision": "eb242db2f2303e0bb06b",
+    "url": "/react_uber-eats/static/js/main.1462984a.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
